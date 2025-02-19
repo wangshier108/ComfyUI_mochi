7b2ea290ca792e185da32be9e29ea81fbad26229
wangshier108 <chihiro_why@163.com>
2025-02-19 11:21:03 +0800

    refine the code

--- mochi.py
+++ mochi.py
@@ -5,35 +5,6 @@ from .utils import (
     read_video_frames
 )
 
-# def send_post_request(api_url, payload, headers):
-#     """
-#     Sends a POST request to the specified API URL with the given payload and headers.
-
-#     Args:
-#         api_url (str): The URL of the API endpoint.
-#         payload (dict): The payload to send in the POST request.
-#         headers (dict): The headers to include in the POST request.
-
-#     Raises:
-#         Exception: If there is an error connecting to the server or the request fails.
-#     """
-#     try:
-#         data = json.dumps(payload).encode("utf-8")
-#         req = urllib.request.Request(api_url, data=data, headers=headers, method="POST")
-#         with urllib.request.urlopen(req) as response:
-#             response_data = response.read().decode("utf-8")
-#         return response_data
-#     except urllib.error.URLError as e:
-#         if "Unauthorized" in str(e):
-#             raise Exception(
-#                 "Key is invalid, please refer to https://cloud.siliconflow.cn to get the API key.\n"
-#                 "If you have the key, please click the 'BizyAir Key' button at the bottom right to set the key."
-#             )
-#         else:
-#             raise Exception(
-#                 f"Failed to connect to the server: {e}, if you have no key, "
-#             )
-
 class mochi:
     API_URL = "http://0.0.0.0:10002/mochivideo"
     @classmethod
